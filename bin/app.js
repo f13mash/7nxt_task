790b8f2edcfeaa760e1d822a91005cb987f7d0a5
Mahesh Gondi <mashf13@gmail.com>
2016-08-09 11:53:58 +0200

    clean up unused packages. feeds are now not stored as part of git commit.

--- bin/app.js
+++ bin/app.js
@@ -15,6 +15,7 @@ require('./../lib/routes/index');
 require('http').createServer(app).listen(app.get('port'), function () {
 
     console.log('Express server listening on port ' + app.get('port'));
+    console.log("Done. Server is ready and waiting for request.");
+    console.log("***Open URL http://localhost:4000/segment/157 ***")
 });
 
-console.log("Done");
--- bin/setup_db.js
+++ bin/setup_db.js
@@ -3,21 +3,51 @@
  */
 ROOT_DIR = __dirname + '/..';
 
+var async = require('async')
 var util = require('util');
+var Promise = require("bluebird");
 var path = require('path')
 var config = require('./../lib/config');
 var fs = require('fs');
 var db_file = path.join(ROOT_DIR, config['db_path']);
-const execSync = require('child_process').execSync;
+var exec = Promise.promisify(require('child_process').exec);
+
+
+function runCommand() {
+
+}
+
+function download_feeds(cb) {
+    //run these commands in parallel
+    console.log("Downloading feeds");
+    feed_load_cmds = [
+        exec(util.format("curl -s https://s3.eu-central-1.amazonaws.com/data-science-homework/data-engineer/homework_rides.csv | tail -n +2 > %s", path.join(ROOT_DIR, "static_db/homework_rides.csv"))),
+        exec(util.format("curl -s https://s3.eu-central-1.amazonaws.com/data-science-homework/data-engineer/homework_segments.csv | tail -n +2 > %s", path.join(ROOT_DIR, "static_db/homework_segments.csv"))),
+        exec(util.format("curl -s https://s3.eu-central-1.amazonaws.com/data-science-homework/data-engineer/homework_route_segments.csv | tail -n +2 > %s", path.join(ROOT_DIR, "static_db/homework_route_segments.csv"))),
+        exec(util.format("curl -s https://s3.eu-central-1.amazonaws.com/data-science-homework/data-engineer/homework_tickets.csv.gz | gunzip | tail -n +2 > %s", path.join(ROOT_DIR, "static_db/homework_tickets.csv")))
+    ]
+    return Promise.all(feed_load_cmds);
+
+}
 
 function init_db() {
-    cleanup();
-    console.log("Initializing Database. This may take a minute or two.");
-    var script_path = "./bin/db_init_script";
+    console.log("Initializing Database. This may take a minute or four.");
+    var script_path = path.join(ROOT_DIR, "./bin/db_init_script");
     var command = util.format("sqlite3 %s < %s", db_file, script_path);
-    var output = execSync(command, {encoding: 'utf-8'})
-    console.log(output);
-    console.log("Database Initialized");
+
+    cleanup();
+    //.then(execSync(command, {encoding: 'utf-8'}))
+    download_feeds()
+        .then(function() {
+            console.log("running sql import");
+            return exec(command);
+        })
+        .then(function () {
+            return exec("rm -rf " + path.join(ROOT_DIR, "static_db/*.csv"));
+        })
+        .then(function() {
+            console.log("Database Initialized");
+        });
 }
 
 function cleanup() {
@@ -31,3 +61,4 @@ if(process.argv[2] && process.argv[2] == 'clean')
     cleanup();
 else
     init_db();
+    //init_db();
