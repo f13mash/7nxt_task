ffb45fd88236e9a45b80396c6668aac581f5bdcd
Mahesh Gondi <mashf13@gmail.com>
2016-08-16 18:25:07 +0200

    config service

--- bin/app.js
+++ bin/app.js
@@ -7,7 +7,7 @@ var app = require('./../lib/server');
 require('./../lib/middlewares/index');
 
 //Setup Routes
-require('./../lib/routes/index');
+require('./../lib/routes');
 
 
 //Start Server
@@ -16,6 +16,6 @@ require('http').createServer(app).listen(app.get('port'), function () {
 
     console.log('Express server listening on port ' + app.get('port'));
     console.log("Done. Server is ready and waiting for request.");
-    console.log("***Open URL http://localhost:4000/segment/157 ***")
+    console.log("***Open URL http://localhost:4000/ ***")
 });
 
